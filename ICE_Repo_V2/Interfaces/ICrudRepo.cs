cf219db37edf6ffa38fce53b5a31ae97c61e8be8
Magnus R. <97102126+MRasch1@users.noreply.github.com>
2025-07-31 09:47:02 +0200

    Interfaces

---
+++ ICE_Repo_V2/Interfaces/ICrudRepo.cs
@@ -0,0 +1,30 @@
+﻿using ICE_Repo_V2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Repo_V2.Interfaces
+{
+    public interface ICrudRepo<Crud>
+    {
+        Crud Create(Crud crud);
+        List<Crud> GetAll();
+        Crud UpdateById(int id, Crud updatedRow);
+        Crud DeleteById(int id);
+    }
+
+    public interface ICountriesRepo : ICrudRepo<Countries> { }
+    public interface ICuisinesRepo : ICrudRepo<Cuisines> { }
+    public interface IDishesRepo : ICrudRepo<Dishes> { }
+    public interface IDishTagRepo : ICrudRepo<DishTag> { }
+    public interface IFavoritesRepo : ICrudRepo<Favorites> { }
+    public interface IIngredientsRepo : ICrudRepo<Ingredients> { }
+    public interface IRecipeIngridientsRepo : ICrudRepo<RecipeIngredients> { }
+    public interface RecipesRepo : ICrudRepo<Recipes> { }
+    public interface ReviewsRepo : ICrudRepo<Reviews> { }
+    public interface TagsRepo : ICrudRepo<Tags> { }
+    public interface UsersRepo : ICrudRepo<Users> { }
+
+}
