76163644663d2262c098c24f9195720928bcc111
Magnus R. <97102126+MRasch1@users.noreply.github.com>
2025-07-31 13:05:39 +0200

    UserRepo

---
+++ ICE_Repo_V2/Context/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using ICE_Repo_V2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Repo_V2.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Countries> Countries { get; set; }
+        public DbSet<Cuisines> Cuisines { get; set; }
+        public DbSet<Dishes> Dishes { get; set; }
+        public DbSet<DishTag> DishTags { get; set; }
+        public DbSet<Favorites> Favorites { get; set; }
+        public DbSet<Ingredients> Ingredients { get; set; }
+        public DbSet<RecipeIngredients> RecipeIngredients { get; set; }
+        public DbSet<Recipes> Recipes { get; set; }
+        public DbSet<Reviews> Reviews { get; set; }
+        public DbSet<Tags> Tags { get; set; }
+        public DbSet<Users> Users { get; set; }
+
+    }
+}
---
+++ ICE_Repo_V2/Repositories/UserRepo.cs
@@ -0,0 +1,61 @@
+﻿using ICE_Repo_V2.Context;
+using ICE_Repo_V2.Interfaces;
+using ICE_Repo_V2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Repo_V2.Repositories
+{
+    public class UserRepo : UsersRepo
+    {
+        private readonly DataContext context;
+        public UserRepo(DataContext data)
+        {
+            this.context = data;
+        }
+
+        public Users Create(Users column)
+        {
+            context.Users.Add(column);
+            context.SaveChanges();
+            return column;
+        }
+
+        public List<Users> GetAll()
+        {
+            return context.Users.ToList();
+        }
+
+        public Users UpdateById(int id, Users updatedColumn)
+        {
+            var column = context.Users.FirstOrDefault(c => c.UserId == id);
+
+            if (column != null)
+            {
+                column.Username = updatedColumn.Username;
+                column.Email = updatedColumn.Email;
+                column.PasswordHash = updatedColumn.PasswordHash;
+
+                context.SaveChanges();
+            }
+
+            return column;
+        }
+
+        public Users DeleteById(int id)
+        {
+            var column = context.Users.FirstOrDefault(c => c.UserId == id);
+
+            if (column != null)
+            {
+                context.Users.Remove(column);
+                context.SaveChanges();
+            }
+            return column;
+        }
+
+    }
+}
