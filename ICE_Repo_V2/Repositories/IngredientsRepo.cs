d930c562f81c0e32ab2b1e78d765028946417722
Magnus R. <97102126+MRasch1@users.noreply.github.com>
2025-08-04 14:08:25 +0200

    ingredientsrepo

--- ICE_Repo_V2/Repositories/IngredientsRepo.cs
+++ ICE_Repo_V2/Repositories/IngredientsRepo.cs
@@ -17,6 +17,39 @@ namespace ICE_Repo_V2.Repositories
             this.context = data;
         }
 
+        public Ingredients Create(Ingredients ingredient)
+        {
+            context.Ingredients.Add(ingredient);
+            context.SaveChanges();
+            return ingredient;
+        }
 
+        public List<Ingredients> GetAll()
+        {
+            return context.Ingredients.ToList();
+        }
+
+        public Ingredients UpdateById(int id, Ingredients updatedIngredient)
+        {
+            var ingredient = context.Ingredients.FirstOrDefault(i => i.IngredientId == id);
+            if (ingredient != null)
+            {
+                ingredient.IngredientName = updatedIngredient.IngredientName;
+                ingredient.Description = updatedIngredient.Description;
+                context.SaveChanges();
+            }
+            return ingredient;
+        }
+
+        public Ingredients DeleteById(int id)
+        {
+            var ingredient = context.Ingredients.FirstOrDefault(i => i.IngredientId == id);
+            if (ingredient != null)
+            {
+                context.Ingredients.Remove(ingredient);
+                context.SaveChanges();
+            }
+            return ingredient;
+        }
     }
 }
